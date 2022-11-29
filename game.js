fe0132094183847f99812763a3527dda588b885d
Retrospective53 <harry.pseu53@gmail.com>
2022-11-29 12:17:15 +0700

    button hightlight computer pick

--- game.js
+++ game.js
@@ -1,16 +1,39 @@
-// Computer random pick 
+// variable
+let computerScore = 0;
+let playerScore = 0;
+let currentRound = 0;
+let playerSelection;
+
+// Button computer pick
+const rockComp = document.querySelector('.rockComp');
+const paperComp = document.querySelector('.paperComp');
+const sciccorsComp = document.querySelector('.sciccorsComp');
+
+// Computer random pick & button hightlight
 function getComputerChoice() {
     const computerOptions = ['rock','paper','sciccors']
     const computerNumber = Math.floor(Math.random()*3);
     move = computerOptions[computerNumber];
+    if (move == 'rock') {
+        rockComp.classList.add('selected');
+        paperComp.classList.remove('selected');
+        sciccorsComp.classList.remove('selected');
+    }
+    else if (move == 'paper') {
+        paperComp.classList.add('selected');
+        rockComp.classList.remove('selected');
+        sciccorsComp.classList.remove('selected');
+    }
+    else if (move == 'sciccors') {
+        sciccorsComp.classList.add('selected');
+        paperComp.classList.remove('selected');
+        rockComp.classList.remove('selected');
     return move;
 }
+}
 
-let computerScore = 0;
-let playerScore = 0;
-let currentRound = 0;
 
-// Button with playRound function when clicked
+// Button with playRound function when clicked (player)
 const btnStart = document.querySelector('.btnStartGame');
 const btnRock = document.querySelector('.rock');
 const btnPaper = document.querySelector('.paper');
@@ -18,6 +41,7 @@ const btnSciccors = document.querySelector('.sciccors');
 btnRock.addEventListener('click', playRound);
 btnPaper.addEventListener('click', playRound);
 btnSciccors.addEventListener('click', playRound);
+
 
 // Display score
 const yourScoreCount = document.querySelector('.yourScoreCount');
@@ -46,7 +70,8 @@ function playRound(playerSelection, computerSelection) {
     
     if (playerSelection == computerSelection) {
         scoreStatusUpdate('Tie');
-        console.log("Tie")
+        console.log("Tie");
+        return;
     }
         
     else if (playerSelection == 'rock') {
